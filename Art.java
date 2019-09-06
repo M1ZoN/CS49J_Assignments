a95f59527e543c73f8913f0b25beb6ab3516ddf9
Mukhammadnozim <31581420+M1ZoN@users.noreply.github.com>
2019-09-06 14:27:32 -0700

    Ascii Art

---
+++ Art.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("                                                  #    #");
+        System.out.println("                                             %%% ##   ##");
+        System.out.println("                                          %%%%% ###%%###");
+        System.out.println("                                         %%%%% ### %%% #");
+        System.out.println("                                       %%%%%% ### %%% ###");
+        System.out.println("                                        %%%% ## %% #######");
+        System.out.println("                                       %%%%% # %% #O#####");
+        System.out.println("                                     %%%%%% # % #########");
+        System.out.println("                                    %%%%% ##### #########");
+        System.out.println("                          ###        %% ####### #########");
+        System.out.println("                 %%% ############    ########### ########");
+        System.out.println("              %%%% ############################### #######");
+        System.out.println("            %%%%% ################################## ######");
+        System.out.println("          %%%%%% #################################### #C###");
+        System.out.println("         %%%%%% #####################################  ###");
+        System.out.println("         %%%%% #######################################");
+        System.out.println("        %%%%%% ########################################");
+        System.out.println("     % %%%%%%% ########################################");
+        System.out.println("      %%%%%%%%% #######################################");
+        System.out.println("     %%%%%%%%%% ########################################");
+        System.out.println("  %%% %%%%%%%%   ###### ################################");
+        System.out.println("    %%%%%%%%      ###### #################### ##########");
+        System.out.println(" % %%%%%%%%        ####### ########### ###### ##########");
+        System.out.println("  %%%%%%%%%         #######  ########### ###### ########");
+        System.out.println(" %%%%%%%%%%          ##### ###  ######### ####### ######");
+        System.out.println("  %%%%%%%%%%          #### ##               ####### ####");
+        System.out.println("  %%%%%%%%%%%           ## #                  ##### ###");
+        System.out.println("   %%  %% % %%         # ##                      ## ###");
+        System.out.println("     %   %    %        # ###                      # ###");
+        System.out.println("                        # ###                     ## ###");
+        System.out.println("                        # ###                     ## ###");
+        System.out.println("                        # ####                   #### ##");
+        System.out.println("                       ### ###                  ##### ###");
+        System.out.println("                      ####  ###                 ####   ##");
+        System.out.println("                     #####   ###                 ##    ##");
+        System.out.println("                    #####    ####                      ###");
+        System.out.println("                     ##        ###                     ###");
+        System.out.println("                                ####                     ##");
+        System.out.println("                                 ####                    ###");
+        System.out.println("                                                         ####");
+        System.out.println("                                                          ##");
+    }
+}
