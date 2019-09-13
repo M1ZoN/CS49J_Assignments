2cb2e1036a2b50ad98abaf90a3d98ce759184d78
Mukhammadnozim <31581420+M1ZoN@users.noreply.github.com>
2019-09-12 17:06:45 -0700

    Update militaryTime.java

--- militaryTime.java
+++ militaryTime.java
@@ -1,41 +1,37 @@
+package com.company;
+
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Please enter the first time: ");
-    int fTime = sc.nextInt();
-    System.out.print("Please enter the second time: ");    
-    int sTime = sc.nextInt();
-    int hour = 0;
-    int min = 0; 
-    fTime = formatTime(fTime);
-    sTime = formatTime(sTime);
-    if (fTime <= sTime)
-    {
-      hour = (sTime - fTime) / 100;
-      min = (sTime - fTime) % 100;
-    }
-    else
-    {
-      hour = (2360 - fTime + sTime) / 100;
-      min = (2360 - fTime + sTime) % 100;
-    }
-    
-    if (min >= 60)
-      {
-        hour++;
-        min %= 60;
-      }
-      System.out.println(hour + " hours " + min + " minutes.");
-  }
+public class E4_17 {
 
-  public static int formatTime(int time)
-  {
-    if (time % 100 == 0)
-    {
-      time = (time / 100 - 1) * 100 + 60;
+    public static void main(String[] args) {
+        // write your code here
+        int nFirstTime = 0;
+        int nSecondTime = 0;
+        int nFirstMinutes = 0;
+        int nSecondMinutes = 0;
+        int nResult = 0;
+        int nMinInDay = 24 * 60;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please enter the first time: ");
+        nFirstTime = sc.nextInt();
+        System.out.print("Please enter the second time: ");
+        nSecondTime = sc.nextInt();
+
+        nFirstMinutes = calculateMinutes(nFirstTime);
+        nSecondMinutes = calculateMinutes(nSecondTime);
+
+        if (nFirstTime <= nSecondTime) {
+            nResult = nSecondMinutes - nFirstMinutes;
+        }
+        else {
+            nResult = nMinInDay - nFirstMinutes + nSecondMinutes;
+        }
+        System.out.println((nResult / 60) + " hours " + (nResult % 60) + " minutes.");
     }
-    return time;
-  }
+
+    private static int calculateMinutes(int nMilitaryTime){
+        return (nMilitaryTime % 100 + nMilitaryTime / 100 * 60);
+    }
 }
